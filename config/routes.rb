ccfda19c84d070e279feba82df16d0ed99f5bc9a
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-02-23 14:43:02 +0000

    [Add]Header,Footer実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root to: 'homes#top'
+ root to: "homes#top"
+ get '/home/about' => 'homes#about'
+ 
+ resources :users, only: [:index,:show,:edit]
+ resources :books, only: [:index, :show, :new, :create, :edit,:update,:destroy]
   
 end
